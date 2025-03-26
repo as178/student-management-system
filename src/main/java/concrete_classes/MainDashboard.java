73e52981316c076df72c7fec4e05f879ca13c3f1
as178 <>
2025-03-26 21:54:51 +1300

    Testing branching

--- src/main/java/concrete_classes/MainDashboard.java
+++ src/main/java/concrete_classes/MainDashboard.java
@@ -14,6 +14,6 @@ package concrete_classes;
 public class MainDashboard {
     public static void main(String[] args) {
         System.out.println("testing git");
-        System.out.println("testing git branches");
+        System.out.println("testing git branch 2");
     }
 }
