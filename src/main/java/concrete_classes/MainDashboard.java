42f2cb023a0e0e863edbdf7ea348223c397c68fd
as178 <>
2025-03-26 20:05:04 +1300

    Folder structure set-up

---
+++ src/main/java/concrete_classes/MainDashboard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package concrete_classes;
+
+/**
+ *
+ * @author Angela Saric (24237573)
+ * 
+ * This is the main dashboard class which showcases login options for
+ * the user.
+ */
+public class MainDashboard {
+    
+}
