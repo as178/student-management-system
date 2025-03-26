f158a141b1eaf2507a1403857e7455e17000542d
as178 <>
2025-03-26 19:31:42 +1300

    First commit

---
+++ src/main/java/pdcproject1group6/student/management/system/StudentManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pdcproject1group6.student.management.system;
+
+/**
+ *
+ * @author Angela Saric (24237573)
+ */
+public class StudentManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
