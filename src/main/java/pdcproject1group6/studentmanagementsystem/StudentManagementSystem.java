bc0c7f5ef78a7f86df7377a98d59d838e13030a8
A S <shadowangel753@gmail.com>
2025-03-26 19:38:20 +1300

    Small change in package name

--- src/main/java/pdcproject1group6/studentmanagementsystem/StudentManagementSystem.java
+++ src/main/java/pdcproject1group6/studentmanagementsystem/StudentManagementSystem.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package pdcproject1group6.student.management.system;
+package pdcproject1group6.studentmanagementsystem;
 
 /**
  *
