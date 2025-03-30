8df5694e8e0c8ea078eea776540e0f18d74dc504
as178 <>
2025-03-31 02:16:26 +1300

    Added more .txt files for potential use and testing Added more usable classes to build and clarify ideas

---
+++ src/main/java/concrete_classes/components/Admin.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package concrete_classes.components;
+
+/**
+ *
+ * @author Angela Saric (24237573)
+ */
+public class Admin {
+    
+}
---
+++ src/main/java/concrete_classes/components/Student.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package concrete_classes.components;
+
+import abstract_classes.User;
+
+/**
+ *
+ * @author Angela Saric (24237573)
+ */
+public class Student extends User {
+
+    //courses lists;
+    
+    public Student(/*input variables EXCLUDING lists*/) {
+    }
+    
+}
